21e105d013285254d5067260a06da9066c6eebe9
smk-141510179 <141510179@smkassalaambandung.sch.id>
2017-02-25 13:50:18 +0700

    akhir

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-success">
+              <center><font size=50>Selamat Datang</font><br></center>
+                    <div class="panel-body">
+                    <center><font size=50>^^Di Sistem Penggajihan^^</font><br></center></div></div></div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
